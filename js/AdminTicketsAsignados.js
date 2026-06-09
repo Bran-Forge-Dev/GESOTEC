a7376c9676817ebeb8888a023c7727ab7e20bc83
Brandon Salinas <mizaelsalinas@outlook.com>
2026-06-08 21:39:27 -0600

    fix: cambiar estado de 'En Progreso' a 'En_Proceso' para cumplir restricción de check

--- js/AdminTicketsAsignados.js
+++ js/AdminTicketsAsignados.js
@@ -173,7 +173,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 },
                 body: JSON.stringify({
                     tecnico_id: tecnicoId,
-                    estado: 'En Progreso'
+                    estado: 'En_Proceso'
                 })
             });
 
